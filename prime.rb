cbafc6b78989a4debb75e10a089704f326fd5948
Michael Ballard <ballard.michael9@gmail.com>
2018-05-07 22:22:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  is_prime = true
+  if num < 2 
+    return false
+  end
+  for i in 2..num-1
+    if num % i == 0 
+      is_prime = false
+    end 
+  end
+  is_prime
+end
