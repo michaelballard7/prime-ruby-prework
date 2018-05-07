318378ca7878a3073d6fb258f460842d50edb2f5
Michael Ballard <ballard.michael9@gmail.com>
2018-05-07 21:43:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,4 @@
 # Add  code here!
 def prime?(num)
-  rangey = [1..num]
-  num_prime = false
-  rangey.each do |x|
-    if num % x == 0
-      num_prime = false 
-    else 
-      num_prime = true
-    end
-  end
-  num_prime
+
 end
