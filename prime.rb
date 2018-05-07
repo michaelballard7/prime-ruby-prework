8ab0190845efe9e4d706de3da90fdb414523ee3e
Michael Ballard <ballard.michael9@gmail.com>
2018-05-07 22:14:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,19 @@
 # Add  code here!
 def prime?(num)
-  if num == 0 or num == 1 or num < 0
-    return false 
-  end
-  i = 2
-  limit = num / i 
-  prime = num % i == 0
-  isPrime = true
-  while i < limit
-   if prime == true
-     isPrime = false
-     return isPrime
+ if n < 5 then 
+   if n == 2 || n == 3 then 
+     return true 
+   else
+     return false 
    end
-   i +=1
-   limit = num /i
-  isPrime
+ end
+ 
+ s = 0 
+ d = n - 1 
+ while(d%2 == 0 ) do
+   d = d/2
+   s = s+1
+   
  end
   
   
