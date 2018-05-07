8a5a3bdd686552577efacb51f55f050091accff6
Michael Ballard <ballard.michael9@gmail.com>
2018-05-07 21:49:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,14 +4,15 @@ def prime?(num)
     return false 
   end
   i = 2 
-  limit = num / (num -1) 
+  limit = num / (num-1) 
   
   while i < limit 
     if num % i == 0 
       return false 
-    end
+    else
     i += 1 
     limit = num / i
+  end
   end
   return true
 end
