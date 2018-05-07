8eda5a464b6db6da0d0dc8c53c4be915912352fa
Michael Ballard <ballard.michael9@gmail.com>
2018-05-07 22:16:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,13 @@
 # Add  code here!
 def prime?(num)
- if n < 5 then 
-   if n == 2 || n == 3 then 
-     return true 
-   else
-     return false 
-   end
- end
+  
+i = 2 
+
+if num < 2 
+  return false
+end
+
+
  
- s = 0 
- d = n - 1 
- while(d%2 == 0 ) do
-   d = d/2
-   s = s+1
-   
- end
-  
-  
+ 
 end
